adb1e9b659961c256b178d393b9fd9f69ad0574d
Sofia Bezertzi <sofia_bezertzi@waters.com>
2023-01-12 16:39:24 +0000

    add: first passing test

---
+++ LeapYear.Tests/LeapYear.Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear.Tests
+{
+    public class LeapYear
+    {
+        [Fact]
+        public void IsLeapYear_Year4_ReturnsTrue()
+        {
+            //arrange
+            var year = 4;
+
+            //act
+            var checkedYear = Year.IsLeapYear(year);
+
+            //asert
+            Assert.True(checkedYear);
+        }
+    }
+}
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LeapYear
+{
+    public class Year
+    {
+        public static void Main() { }
+
+        public static bool IsLeapYear(int year)
+        {
+            return true;
+        }
+    }
+}
